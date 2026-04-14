68d228221a333fdf3823fcbfc6e52385fe78c0da
rma-toni <rmaflores.sw@gmail.com>
2026-04-14 22:38:32 +0000

    2 finish

--- tp1/punto2.cpp
+++ tp1/punto2.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <chrono>
 
 using namespace std;
 
 int main(){
-    int n, counter = 0;
-    vector<int> numeros;
-
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distr(0, 10000);
+    int n;
 
     cout << "Ingrese un numero n mayor a 10^5: ";
+
     while (true)
     {
         if (cin >> n && n >= 1e5) break;
@@ -23,9 +20,31 @@ int main(){
         }
     }
 
+    auto inicio = chrono::high_resolution_clock::now();
+
+    vector<int> numeros;
+    int counter = 0;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distr(0, 10000);
+
     for (int i = 0; i < n; i++)
     {
         numeros.push_back(distr(gen));
     }
-    
+
+    int x = 5; // entrada por usuario? (cin)
+    for (int num : numeros)
+    {
+        if (num == x)
+        {
+            counter++;
+        }
+    }
+    cout << "el numero se encontro " << counter << " veces" << endl;
+    auto fin = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> tiempo_transcurrido = fin - inicio;
+
+    cout << "Resultados del analisis:" << endl;
+    cout << "El proceso tardo: " << tiempo_transcurrido.count() << " ms" << endl;
 }
